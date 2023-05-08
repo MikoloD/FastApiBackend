531f1b17d771a5970cb6eec57685ba34603b2c6f
MikoloD <grygiel.mikolaj@gmail.com>
2023-05-08 22:58:17 +0200

    Update song.py

--- Models/song.py
+++ Models/song.py
@@ -1,11 +1,13 @@
 
-
 class Model:
-    def __init__(self, artist, name, songId):
+    def __init__(self, artist, name, songId, spotifyUri):
         self.artist = artist
         self.name = name
         self.songId = songId
-
+        self.spotifyUri = spotifyUri
+    def UriReplace(self):
+        self.spotifyUri = self.spotifyUri.replace("spotify", "spotify" + ".com")
+        self.spotifyUri = 'https://open.'+ self.spotifyUri.replace(":", "/")
 
 def encode(self):
     return self.__dict__
